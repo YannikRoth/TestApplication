6a7d6c360341428546d78d76eb83619a4c4381c1
Yannik Roth <roth.yannik@hotmail.com>
2020-03-28 17:43:52 +0100

    test

--- app/src/main/java/com/example/testapplication/YanniksClass.java
+++ app/src/main/java/com/example/testapplication/YanniksClass.java
@@ -3,6 +3,6 @@ package com.example.testapplication;
 public class YanniksClass {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
 }
